0a6bd52f541d99f84c438493125c222660c44c0c
Aaron Quint <aaron@quirkey.com>
2010-05-16 00:46:58 -0400

    Working path substituion

--- lib/soca/push.rb
+++ lib/soca/push.rb
@@ -4,7 +4,7 @@ module Soca
     attr_reader :config
     
     def initialize(app_dir, config_path = nil)
-      self.app_dir     = app_dir
+      self.app_dir     = File.expand_path(app_dir) + '/'
       self.config_path = config_path
       load_config
     end
@@ -18,6 +18,40 @@ module Soca
     end
     
     def build
+      final_hash = {}
+      Dir.glob(app_dir + '**/**') do |path|
+        next if File.directory?(path)
+        final_hash = map_file(path, final_hash)
+      end
+      final_hash
+    end
+    
+    private
+    def map_file(path, hash)
+      file_data = File.read(path)
+      base_path = path.gsub(app_dir, '')
+      if map = mapped_directories.detect {|k,v| k =~ base_path }
+        base_path = base_path.gsub(map[0], map[1])
+      end
+      parts = base_path.gsub(/^\//, '').split('/')
+      current_hash = hash
+      while !parts.empty?
+        part = parts.shift
+        if parts.empty?
+          current_hash[part] = file_data
+        else
+          current_hash[part] ||= {}
+          current_hash = current_hash[part]
+        end
+      end
+      hash
+    end
+    
+    def mapped_directories
+      return @mapped_directories if @mapped_directories
+      map = {}
+      config['mapDirectories'].collect {|k,v| map[/^#{k}/] = v }
+      @mapped_directories = map
     end
     
   end
