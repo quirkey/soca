28b8a04c206975fd2871a5ed7b0bd4c5ea21574b
Aaron Quint <aaron@quirkey.com>
2010-10-04 08:21:12 -0700

    Update history

--- lib/soca.rb
+++ lib/soca.rb
@@ -7,7 +7,7 @@ require 'logger'
 $LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__))))
 
 module Soca
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 
   class << self
     attr_accessor :debug
