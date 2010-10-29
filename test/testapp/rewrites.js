58bf4033a559bb555651f8080aba02c739bcbc6d
Richard Metzler <richard@eaglefeed.me>
2010-10-29 15:03:14 +0200

    keeping relative urls sane

--- test/testapp/rewrites.js
+++ test/testapp/rewrites.js
@@ -1,8 +1,15 @@
+/**
+ * this file contains the json representation for rewrite rules 
+**/
 [
-  {
+  { // rewriting / to index.html
     "from":"/",
     "to":"index.html",
     "method":"GET",
     "query":{}
+  },
+  { // keeping relative urls sane
+    "from":"/*",
+    "to":"/*"
   }
 ]
