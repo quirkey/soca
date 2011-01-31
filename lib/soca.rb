9904164dbd5c38e02a9d54cad2fa66008ae7e90f
narkisr <narkisr.dev@gmail.com>
2011-02-01 05:13:50 +0800

    macro works

--- lib/soca.rb
+++ lib/soca.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require 'json'
 require 'typhoeus'
 require 'base64'
--- test/test_plugins.rb
+++ test/test_plugins.rb
@@ -4,13 +4,15 @@ class TestSocaPusher < Test::Unit::TestCase
 
   context "macro plugin" do
     setup do
-      @push = Soca::Pusher.new(@test_app_dir)
+      @pusher = Soca::Pusher.new(@test_app_dir)
     end
-   
-    should "sanity" do
-      @push.push!
+
+    should "replace !code macros with file contents" do
+        @pusher.push!
+        sammy = File.read("#{@test_app_dir}/js/vendor/sammy-0.5.4.js")
+        assert @pusher.document['views']['recent']['map'].include?(sammy)
     end
 
 
-   end
+  end
 end
