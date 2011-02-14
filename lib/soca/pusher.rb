3d4cfb79eb978ec04296d6a72d9ad288479fa353
Brian Candler <b.candler@pobox.com>
2011-02-15 06:18:57 +0800

    Display 'put' errors when not in debug mode

--- lib/soca/pusher.rb
+++ lib/soca/pusher.rb
@@ -177,7 +177,11 @@ module Soca
       logger.debug "PUT #{url}"
       logger.debug "body: #{body[0..80]} ..."
       response = Typhoeus::Request.put(url, :body => body)
-      logger.debug "Response: #{response.code} #{response.body[0..200]}"
+      if response.success? || response.code == 412
+        logger.debug "Response: #{response.code} #{response.body[0..200]}"
+      else
+        logger.error "Response: #{response.code} #{response.body[0..200]}"
+      end
       response
     end
 
