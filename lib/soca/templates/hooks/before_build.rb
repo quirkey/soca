5eacca915f04b157bc136175757ee4872133b33f
Aaron Quint <aaron@quirkey.com>
2012-02-04 13:34:29 -0800

    Fix the generated template for before_build hook. Fixes #32

--- lib/soca/templates/hooks/before_build.rb
+++ lib/soca/templates/hooks/before_build.rb
@@ -1,3 +1,5 @@
-# any pre build scripting or plugins should go here
-plugin 'coffeescript'
-plugin 'jim'
+# any pre build scripting should go here. As of v0.3 plugins are defined in config.json
+
+# The context here is the Soca::Pusher so you have access to things like the `env` and `document`
+# It's also already chdir'd into the application dir so you can manipulate the document
+# or the local files
